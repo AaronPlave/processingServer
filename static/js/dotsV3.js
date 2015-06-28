1329d203a05c42aabf16a67dd5d76a6eaa895423
AaronPlave <aplave@wesleyan.edu>
2015-06-28 11:49:40 -0400

    changed jquery to local source for airplane fun times. Added in ability to choose from 2 and 3 colored themes. Themes live on frontend and just define what colors to set on backend.

--- static/js/dotsV3.js
+++ static/js/dotsV3.js
@@ -39,9 +39,168 @@ var pHandler;
 var _opts;
 var gui;
 var folders = [];
+var fFill;
 var viewMode = "create";
 var canvasRef;
 var frameRateEl;
+var fillColorThemes = {
+    "(2) Complementary RO BG": [
+        [240, 90, 34, 1],
+        [109, 199, 190, 1]
+    ],
+    "(2) Complementary O B": [
+        [245, 138, 31, 1],
+        [0, 172, 220, 1]
+    ],
+    "(2) Complementary YO BV": [
+        [252, 184, 19, 1],
+        [82, 79, 160, 1],
+    ],
+    "(2) Complementary Y V": [
+        [252, 238, 30, 1],
+        [105, 61, 141, 1],
+    ],
+    "(3) Russian Poster Art 1": [
+        [255, 229, 110, 1],
+        [215, 25, 32, 1],
+        [0, 0, 0, 1]
+    ],
+    "(3) Russian Poster Art 2": [
+        [240, 90, 34, 1],
+        [255, 244, 222, 1],
+        [80, 133, 181, 1]
+    ],
+    "(3) Russian Poster Art 7": [
+        [226, 166, 92, 1],
+        [221, 209, 222, 1],
+        [35, 31, 32, 1]
+    ],
+    "(3) Russian Poster Art 8": [
+        [255, 218, 137, 1],
+        [132, 170, 142, 1],
+        [236, 28, 36, 1]
+    ],
+    "(3) Pop Art 2": [
+        [58, 77, 128, 1],
+        [215, 25, 33, 1],
+        [255, 255, 255, 1]
+    ],
+    "(3) Pop Art 4": [
+        [252, 230, 0, 1],
+        [83, 193, 185, 1],
+        [236, 28, 36, 1]
+    ],
+    "(3) Desaturated 2": [
+        [83, 105, 122, 1],
+        [216, 208, 62, 1],
+        [194, 138, 109, 1]
+    ],
+    "(3) Desaturated 6": [
+        [176, 191, 200, 1],
+        [128, 157, 172, 1],
+        [70, 112, 129, 1]
+    ],
+    "(3) Saturated 2": [
+        [240, 96, 34, 1],
+        [25, 187, 220, 1],
+        [205, 144, 45, 1]
+    ],
+    "(3) Saturated 3": [
+        [255, 221, 0, 1],
+        [94, 180, 70, 1],
+        [9, 76, 160, 1]
+    ],
+    "(3) Saturated 4": [
+        [210, 188, 42, 1],
+        [0, 182, 193, 1],
+        [211, 64, 136, 1]
+    ],
+    "(3) Saturated 5": [
+        [255, 221, 0, 1],
+        [0, 105, 180, 1],
+        [236, 28, 36, 1]
+    ],
+    "(3) Warm 2": [
+        [252, 238, 30, 1],
+        [233, 55, 67, 1],
+        [243, 158, 79, 1]
+    ],
+    "(3) Warm 4": [
+        [165, 76, 44, 1],
+        [244, 178, 48, 1],
+        [167, 185, 58, 1]
+    ],
+    "(3) Corporate 2": [
+        [0, 117, 177, 1],
+        [163, 194, 218, 1],
+        [35, 31, 32, 1]
+    ],
+    "(3) Corporate 4": [
+        [253, 233, 63, 1],
+        [2, 143, 206, 1],
+        [35, 31, 32, 1]
+    ],
+    "(3) eCommerce 1": [
+        [253, 233, 63, 1],
+        [0, 178, 189, 1],
+        [97, 84, 163, 1]
+    ],
+    "(3) Industrial 1": [
+        [175, 166, 143, 1],
+        [126, 115, 91, 1],
+        [218, 215, 196, 1]
+    ],
+    "(3) Industrial 3": [
+        [128, 157, 172, 1],
+        [170, 172, 175, 1],
+        [230, 230, 224, 1]
+    ],
+    "(3) Orange Juice": [
+        [251, 187, 34, 1],
+        [245, 180, 84, 1],
+        [237, 149, 33, 1]
+    ],
+    "(3) Citrus 2": [
+        [255, 195, 12, 1],
+        [238, 124, 50, 1],
+        [240, 95, 91, 1]
+    ],
+    "(3) Lollipop 2": [
+        [255, 197, 10, 1],
+        [86, 82, 162, 1],
+        [41, 171, 226, 1]
+    ],
+    "(3) Analogous V RV R": [
+        [101, 45, 144, 1],
+        [182, 36, 103, 1],
+        [236, 28, 36, 1]
+    ],
+    "(3) Analogous O YO Y": [
+        [245, 138, 31, 1],
+        [252, 184, 19, 1],
+        [252, 238, 30, 1]
+    ],
+    "(3) Analogous R RO O": [
+        [236, 28, 36, 1],
+        [240, 90, 34, 1],
+        [245, 138, 31, 1]
+    ],
+    "(3) Split Complementary O BG BV": [
+        [245, 138, 31, 1],
+        [109, 199, 190, 1],
+        [82, 79, 160, 1]
+    ],
+    "(3) Split Complementary YO B V": [
+        [252, 184, 19, 1],
+        [0, 172, 220, 1],
+        [101, 45, 144, 1]
+    ],
+    "(3) Split Complementary BG R O": [
+        [109, 199, 190, 1],
+        [236, 28, 36, 1],
+        [245, 138, 31, 1]
+    ]
+}
 
 function initialize() {
     var canvasHolder = document.getElementById("canvasHolder");
@@ -96,10 +255,11 @@ function initGui() {
         this.U_DOT_SINGLE_STROKE_COLOR = pHandler.colorToRGB(pOpt.U_DOT_SINGLE_STROKE_COLOR);
         this.U_DOT_SINGLE_STROKE_COLOR_OPACITY = pHandler.colorToRGB(pOpt.U_DOT_SINGLE_STROKE_COLOR)[3];
         this.U_DOT_FILL = pOpt.U_DOT_FILL;
-        this.U_DOT_FILL_NUM_COLORS = pHandler.colorToRGB(pOpt.U_DOT_FILL_NUM_COLORS);
+        this.U_DOT_FILL_NUM_COLORS = pOpt.U_DOT_FILL_NUM_COLORS;
         this.U_DOT_FILL_COLOR_1 = pHandler.colorToRGB(pOpt.U_DOT_FILL_COLOR_1);
         this.U_DOT_FILL_COLOR_2 = pHandler.colorToRGB(pOpt.U_DOT_FILL_COLOR_2);
         this.U_DOT_FILL_COLOR_3 = pHandler.colorToRGB(pOpt.U_DOT_FILL_COLOR_3);
+        this.U_DOT_FILL_THEMES = "";
         // this.U_DOT_SINGLE_FILL_COLOR_OPACITY = pHandler.colorToRGB(pOpt.U_DOT_SINGLE_FILL_COLOR)[3];
         this.U_DOT_OFFSET_X_MAX = pOpt.U_DOT_OFFSET_X_MAX;
         this.U_DOT_OFFSET_Y_MAX = pOpt.U_DOT_OFFSET_Y_MAX;
@@ -163,13 +323,13 @@ function initGui() {
     });
 
     // FILL
-    var fFill = gui.addFolder('Fill');
+    fFill = gui.addFolder('Fill');
     folders.push(fFill);
     var cFill = fFill.add(_opts, 'U_DOT_FILL').name("Enabled");
     cFill.onChange(function(value) {
         pHandler.getUiOpt().U_DOT_FILL = value;
     });
-    var cFillNumColors = fFill.add(_opts, 'U_DOT_FILL_NUM_COLORS',[1,2,3]).name("# Fill Colors");
+    var cFillNumColors = fFill.add(_opts, 'U_DOT_FILL_NUM_COLORS', [1, 2, 3]).name("# Fill Colors");
     cFillNumColors.onChange(function(value) {
         pHandler.setFillNumColors(value);
     });
@@ -180,13 +340,13 @@ function initGui() {
             if (value.indexOf("#") === 0) {
                 // Case where alpha is set to 1, the color
                 // selector reverts to hex... Of course.
-                pHandler.setFillColor(hexToRgb(value),0);
+                pHandler.setFillColor(hexToRgb(value), 0);
             } else {
-                pHandler.setFillColor(rgbaStringToList(value),0);
+                pHandler.setFillColor(rgbaStringToList(value), 0);
             }
         } else {
             // Case where it returns a list (object type).
-            pHandler.setFillColor(value,0);
+            pHandler.setFillColor(value, 0);
         }
     });
     var cFillColor2 = fFill.addColor(_opts, 'U_DOT_FILL_COLOR_2').name("Color 2");
@@ -195,13 +355,13 @@ function initGui() {
             if (value.indexOf("#") === 0) {
                 // Case where alpha is set to 1, the color
                 // selector reverts to hex... Of course.
-                pHandler.setFillColor(hexToRgb(value),1);
+                pHandler.setFillColor(hexToRgb(value), 1);
             } else {
-                pHandler.setFillColor(rgbaStringToList(value),1);
+                pHandler.setFillColor(rgbaStringToList(value), 1);
             }
         } else {
             // Case where it returns a list (object type).
-            pHandler.setFillColor(value,1);
+            pHandler.setFillColor(value, 1);
         }
     });
     var cFillColor3 = fFill.addColor(_opts, 'U_DOT_FILL_COLOR_3').name("Color 3");
@@ -210,23 +370,35 @@ function initGui() {
             if (value.indexOf("#") === 0) {
                 // Case where alpha is set to 1, the color
                 // selector reverts to hex... Of course.
-                pHandler.setFillColor(hexToRgb(value),2);
+                pHandler.setFillColor(hexToRgb(value), 2);
             } else {
-                pHandler.setFillColor(rgbaStringToList(value),2);
+                pHandler.setFillColor(rgbaStringToList(value), 2);
             }
         } else {
             // Case where it returns a list (object type).
-            pHandler.setFillColor(value,2);
+            pHandler.setFillColor(value, 2);
         }
     });
-    // var cFillSingleColorOpacity = fFill.add(_opts, 'U_DOT_SINGLE_FILL_COLOR_OPACITY', 0, 255).step(1).name("Color Opacity");
-    // cFillSingleColorOpacity.onChange(function(value) {
-    //     pHandler.setSingleFillColorOpacity(value);
-    // });
-    // var cFillTheme = fFill.add(_opts, 'U_DOT_FILL_THEME').name("Enable Theme");
-    // cFillTheme.onChange(function(value) {
-    //     pHandler.setTheme(value);
-    // });
+
+    var cFillThemes = fFill.add(_opts, 'U_DOT_FILL_THEMES', Object.keys(fillColorThemes)).name("Fill Themes");
+    cFillThemes.onChange(function(value) {
+        var t = fillColorThemes[value];
+        if (t === null) {
+            console.log("Theme could not be loaded");
+            alert("Theme could not be loaded");
+            return;
+        }
+        if (t.length > 0) {
+            pHandler.setFillColor(t[0], 0);
+        }
+        if (t.length > 1) {
+            pHandler.setFillColor(t[1], 1);
+        }
+        if (t.length > 2) {
+            pHandler.setFillColor(t[2], 2);
+        }
+        updateColors();
+    });
 
     // STROKE
     var fStroke = gui.addFolder('Stroke');
@@ -390,10 +562,11 @@ function resetControls() {
     _opts.U_DOT_SINGLE_STROKE_COLOR = pHandler.colorToRGB(pOpt.U_DOT_SINGLE_STROKE_COLOR);
     // _opts.U_DOT_SINGLE_STROKE_COLOR_OPACITY = pHandler.colorToRGB(pOpt.U_DOT_SINGLE_STROKE_COLOR)[3];
     _opts.U_DOT_FILL = pOpt.U_DOT_FILL;
-    _opts.U_DOT_FILL_NUM_COLORS = pHandler.colorToRGB(pOpt.U_DOT_FILL_NUM_COLORS);
+    _opts.U_DOT_FILL_NUM_COLORS = pOpt.U_DOT_FILL_NUM_COLORS;
     _opts.U_DOT_FILL_COLOR_1 = pHandler.colorToRGB(pOpt.U_DOT_FILL_COLOR_1);
     _opts.U_DOT_FILL_COLOR_2 = pHandler.colorToRGB(pOpt.U_DOT_FILL_COLOR_2);
     _opts.U_DOT_FILL_COLOR_3 = pHandler.colorToRGB(pOpt.U_DOT_FILL_COLOR_3);
+    _opts.U_DOT_FILL_THEMES = "";
     _opts.U_DOT_OFFSET_X_MAX = pOpt.U_DOT_OFFSET_X_MAX;
     _opts.U_DOT_OFFSET_Y_MAX = pOpt.U_DOT_OFFSET_Y_MAX;
     _opts.U_DOT_RADIUS = pOpt.U_DOT_RADIUS;
@@ -408,7 +581,17 @@ function resetControls() {
             fc[j].updateDisplay();
         }
     }
+}
 
+function updateColors() {
+    var pOpt = pHandler.getUiOpt();
+    _opts.U_DOT_FILL_COLOR_1 = pHandler.colorToRGB(pOpt.U_DOT_FILL_COLOR_1);
+    _opts.U_DOT_FILL_COLOR_2 = pHandler.colorToRGB(pOpt.U_DOT_FILL_COLOR_2);
+    _opts.U_DOT_FILL_COLOR_3 = pHandler.colorToRGB(pOpt.U_DOT_FILL_COLOR_3);
+    var fc = fFill.__controllers;
+    for (var j = 0; j < fc.length; j++) {
+        fc[j].updateDisplay();
+    }
 }
 
 var colorVars = ["U_BG_COLOR", "U_DOT_SINGLE_STROKE_COLOR", "U_DOT_SINGLE_FILL_COLOR"];
