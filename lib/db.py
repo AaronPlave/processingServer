62121630663972904ec23dfbb2b92ff3a64b46b8
AaronPlave <aplave@wesleyan.edu>
2015-09-26 11:51:44 -0700

    Adding tmp conversion function from existing URI to png

--- lib/db.py
+++ lib/db.py
@@ -1,4 +1,6 @@
 import pymongo
+from PIL import Image
+from io import BytesIO
 
 client = pymongo.MongoClient()
 db = client["dots"]
@@ -17,22 +19,37 @@ def getSketchById(id):
 		return {"success":False,"error":"Multiple sketches with same id"}
 
 def getAllSketches():
-	allSketches = ALL_SKETCHES.find({"id":"allSketches"})
+	allSketches = SKETCHES.find({"id":"*"},{id:1,sketch:0,_id:0,thumbnail:1})
 	if allSketches.count() == 0:
 		return {}
-	return allSketches[0].get("allSketches")
+	return allSketches
+
+def convertAll():
+	allSketches = SKETCHES.find()
+	for i in allSketches:
+		thb = i["thumbnail"]
+		rId = i["id"]
+		filepath = "static/imgs/"+rId+".png"
+		data = uri.split("base64,")[1]
+	    im = Image.open(BytesIO(base64.b64decode(data)))
+	    im.save(filepath)
+	    if not filepath:
+	    	print "UH NO",rId
+	    	return
+
+		i.update({"id":"allSketches"},{"$set":{"thumbnail":filepath}},upsert=True)
 
 def addSketch(id,sketch,thumbnail):
 	try:
 		add = SKETCHES.insert({"id":id, "sketch": sketch, "thumbnail": thumbnail})
-		allSketches = ALL_SKETCHES.find()
-		data = {id:thumbnail}
-		if allSketches.count() == 0:
-			ALL_SKETCHES.insert({"id":"allSketches","allSketches":data})
-		else:
-			oldSketches = allSketches[0]["allSketches"]
-			oldSketches[id] = thumbnail
-			ALL_SKETCHES.update({"id":"allSketches"},{"$set": {"id":"allSketches","allSketches":oldSketches}}, upsert=True)
+		# allSketches = ALL_SKETCHES.find()
+		# data = {id:thumbnail}
+		# if allSketches.count() == 0:
+		# 	ALL_SKETCHES.insert({"id":"allSketches","allSketches":data})
+		# else:
+		# 	oldSketches = allSketches[0]["allSketches"]
+		# 	oldSketches[id] = thumbnail
+			# ALL_SKETCHES.update({"id":"allSketches"},{"$set": {"id":"allSketches","allSketches":oldSketches}}, upsert=True)
 	except Exception, e:
 		print "ERROR: Could not insert"
 		print e
